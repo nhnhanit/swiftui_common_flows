5a85c9df6104dc32e58b68a9f7c51d76fe8693be
Nguyen Hong Nhan <nhnhanit@gmail.com>
2025-04-28 16:16:52 +0700

    Message: - fix error route

--- SwiftUI_0325/Modules/Post/Coordinator/PostCoordinator.swift
+++ SwiftUI_0325/Modules/Post/Coordinator/PostCoordinator.swift
@@ -23,22 +23,3 @@ class PostCoordinator: ObservableObject {
     }
     
 }
-
-enum PostRoute: Hashable {
-    
-    case postDetail(postId: Int, detailVM: PostDetailViewModel)
-    
-    static func == (lhs: PostRoute, rhs: PostRoute) -> Bool {
-        switch (lhs, rhs) {
-        case let (.postDetail(p1, _), .postDetail(p2, _)):
-            return p1 == p2
-        }
-    }
-
-    func hash(into hasher: inout Hasher) {
-        switch self {
-        case let .postDetail(id, _):
-            hasher.combine(id)
-        }
-    }
-}
